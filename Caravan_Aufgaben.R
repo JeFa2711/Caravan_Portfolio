52dd11b998dac55a7e3c692551c0d8a830751f6e
Jessi <jessica.fndr@gmail.com>
2022-06-06 21:54:35 +0200

    File created

---
+++ Caravan_Aufgaben.R
@@ -0,0 +1,50 @@
+#
+# Template für die Bearbeitung der Übungsaufgabe 5
+#        -- Analyse des Caravan-Datensatz -- 
+#
+
+#-----------------
+# Bitte ausfüllen:
+#-----------------
+# Gruppenname: Anakin
+# Gruppenteilnehmer: Nina Heyer, Jonas Lang, Simon Baader, Jessica Fander
+
+
+
+#-------------------------
+# notwendinge Bibliotheken
+#-------------------------
+library(ISLR) #Datensatz
+
+
+#------------------------------------
+# Aufgabe 1: Evaluierungsmetrik
+#------------------------------------
+
+#    ...Platz für Ihre Antwort / Ihren Code...
+
+#------------------------------------
+# Aufgabe 2: Trainings- und Testdaten
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 3: Logistische Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 4: KNN
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 5: Modellauswahl
+#------------------------------------
+
+#    ...Platz für Ihren Code, Auswahl und Begründung...
+
+
