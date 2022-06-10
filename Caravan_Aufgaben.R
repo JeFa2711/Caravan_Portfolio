4b3a6eca6469b72d7d8e85c9493e8d996d426a0d
Jessi <jessica.fndr@gmail.com>
2022-06-10 16:21:59 +0200

    comm

--- Caravan_Aufgaben.R
+++ Caravan_Aufgaben.R
@@ -19,7 +19,17 @@ plot(Caravan$Purchase)
 #------------------------------------
 # Aufgabe 1: Evaluierungsmetrik
 #------------------------------------
-
+# In der Aufgabenstellung ist beschrieben, dass die Faktoren Recall 
+# (tats. interessierten Kunden) und Precision (Minimierung der False Positives) für den 
+# Sachverhalt relevant sind.
+# Aus der gegebenen Aufgabe lässt sich das Kostenverhältnis zwischen nicht-identifizierten 
+# Kunden und verschwendeter Zeit der Mitarbeiter in uninteressierte Kunden nicht schließen. 
+# Daher wird als Evaluierungsmetrik keine Kostenfunktion, sondern die F1-Metrik als 
+# harmonisches Mittel von Recall und Precision verwendet, um den optimalsten Mittelwert 
+# zwischen beiden Metriken festzustellen.
+# Mit konkreteren Informationen zum gegebenen Sachverhalt könnte sich eine sinnvolle 
+# Kostenfunktion ableiten lassen, mit der sich die Effiktivität der Versicherung 
+# (besser als mit der F1-Metrik) optimieren ließe.
 
 #------------------------------------
 # Aufgabe 2: Trainings- und Testdaten
@@ -30,6 +40,7 @@ plot(Caravan$Purchase)
 
 set.seed(28)
 laenge <- length(Caravan$Purchase)
+anzahlSpalten <- length(Caravan)
 aufteilung <- sample(laenge,0.8*laenge)
 Caravan.trainingsdaten <- Caravan[aufteilung,]
 Caravan.testdaten <- Caravan[-aufteilung,]
@@ -37,8 +48,6 @@ Caravan.testdaten <- Caravan[-aufteilung,]
 #------------------------------------
 # Aufgabe 3: Logistische Regression
 #------------------------------------
-
-#    ...Platz für Ihren Code...
 # Logistische Regression
 glm.fit <- glm(
   formula = Purchase ~ . ,
@@ -53,13 +62,15 @@ vorhergeseheneWerte <- predict(
   newdata = Caravan.testdaten, 
   type    = "response"
 )
-#Aus diesen vorhergesehenen Werten, werden nun cutoffPoints definiert. Um sinnvolle Punkte festzustellen,
+
+# Aus diesen vorhergesehenen Werten, werden nun cutoffPoints definiert. Um sinnvolle Punkte festzustellen,
 # werden die Vorhergesagten Werte (transparent) geplottet, um ihre Verteilung festzustellen
 
 plot(vorhergeseheneWerte, col = adjustcolor("blue", alpha = 0.35))
 
 # Aus diesen Werten wurden nun,nach dem eigenem ermessen der Mitarbeiter Cut-Off points definiert 
-# Der Fokus liegt hierbei im Bereich 0-01-0.1, da der Plot aufzeigt, dass hier die meisten Werte zu finden sind 
+# Der Fokus liegt hierbei im Bereich 0-01-0.1, da der Plot aufzeigt, dass hier die meisten Werte zu finden sind.
+
 t <- c(0.01,0.05,0.07,0.1, 0.2)
 wahrheitsMatrix <- list()
 
@@ -76,7 +87,7 @@ for ( i in 1:length(t)) {
 #------------------------------------
 library(class)
 
-#Für die Verwendung in KNN, müssen die Testdaten in data.frames umgewandelt werden 
+#Für die Verwendung in KNN, müssen die Testdaten in data.frames umgewandelt werden. 
 Caravan.scaled = subset(Caravan, select = -c(Purchase) )
 
 scale(Caravan.scaled)
@@ -85,23 +96,53 @@ Caravan.scaled.testdaten <- as.data.frame(Caravan.scaled[-aufteilung,])
 Caravan.scaled.trainingsdaten.output <- Caravan[aufteilung, "Purchase"]
 Caravan.scaled.testdaten.output <- Caravan[-aufteilung, "Purchase"]
 set.seed(28)
+
 # K = 3
 knn.pred.k3 <- knn(
   train = Caravan.scaled.trainingsdaten,         ## Input-Variablen der Trainingsdaten      
-  test  = Caravan.scaled.testdaten,          ## Input-Variablen der Testdaten
-  cl    = Caravan.scaled.trainingsdaten.output, ## Class-Label der Trainingsdaten
+  test  = Caravan.scaled.testdaten,              ## Input-Variablen der Testdaten
+  cl    = Caravan.scaled.trainingsdaten.output,  ## Class-Label der Trainingsdaten
   k     = 3,
   prob = FALSE
 ) 
 
 confusion.table.k3 <- table(knn.pred.k3,Caravan.scaled.testdaten.output)
+confusion.table.k3
 
-#    ...Platz für Ihren Code...
+# K=4
+knn.pred.k4 <- knn(
+  train = Caravan.scaled.trainingsdaten,         ## Input-Variablen der Trainingsdaten      
+  test  = Caravan.scaled.testdaten,              ## Input-Variablen der Testdaten
+  cl    = Caravan.scaled.trainingsdaten.output,  ## Class-Label der Trainingsdaten
+  k     = 4,
+  prob = FALSE
+) 
+
+confusion.table.k4 <- table(knn.pred.k4,Caravan.scaled.testdaten.output)
+confusion.table.k4
+
+#K=5
+knn.pred.k5 <- knn(
+  train = Caravan.scaled.trainingsdaten,         ## Input-Variablen der Trainingsdaten      
+  test  = Caravan.scaled.testdaten,              ## Input-Variablen der Testdaten
+  cl    = Caravan.scaled.trainingsdaten.output,  ## Class-Label der Trainingsdaten
+  k     = 5,
+  prob = FALSE
+) 
+
+confusion.table.k5 <- table(knn.pred.k5,Caravan.scaled.testdaten.output)
+confusion.table.k5
+
 
 #------------------------------------
 # Aufgabe 5: Modellauswahl
 #------------------------------------
 
-#    ...Platz für Ihren Code, Auswahl und Begründung...
+# Ziel laut Aufgabenstellung ist es, möglichst wenige false positives zu erhalten,
+# um die Arbeitszeit nicht zu verschwenden. Andererseits soll es auch möglichst viele Personen erkennen,
+# die Interesse an einer Police haben, um diese zu verkaufen.
+# Zusammenfassend die Übersicht: 
+# Logistische Regression: 
 
+library (ROCR)
 
