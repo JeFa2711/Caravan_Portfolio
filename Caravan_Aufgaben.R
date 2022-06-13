fea0fb46fc4c5994374a77ce02f95930a27e659e
Jessi <jessica.fndr@gmail.com>
2022-06-13 11:05:17 +0200

    Begründungen ergänzt

--- Caravan_Aufgaben.R
+++ Caravan_Aufgaben.R
@@ -11,10 +11,17 @@
 #-------------------------
 # notwendinge Bibliotheken
 #-------------------------
-library(ISLR) #Datensatz
+#Datensatz
+library(ISLR) 
+library(class)
+
 ## Übersicht
 summary(Caravan)
-plot(Caravan$Purchase)
+plot(Caravan$Purchase) # Plotten von Yes und No 
+anzahlYes <- nrow(Caravan[Caravan$Purchase == "Yes",])
+anzahlYes # 348
+# Anzahl No: 5474
+# 348 von 5822 haben eine Versicherung (ca. 6%)
 
 #------------------------------------
 # Aufgabe 1: Evaluierungsmetrik
@@ -38,12 +45,12 @@ plot(Caravan$Purchase)
 # 80% (Trainingsdaten) von 5822 sind ~4658 
 # Entsprechend dazu 1164 Datensätze für die Testdaten 
 
-set.seed(28)
-laenge <- length(Caravan$Purchase)
-anzahlSpalten <- length(Caravan)
-aufteilung <- sample(laenge,0.8*laenge)
-Caravan.trainingsdaten <- Caravan[aufteilung,]
-Caravan.testdaten <- Caravan[-aufteilung,]
+set.seed(28) # Für bessere Nachvollziehbarkeit wird ein Seed gesetzt.
+laenge <- length(Caravan$Purchase) # Anzahl der Datensätze
+anzahlSpalten <- length(Caravan) # Anzahl der Spalten
+aufteilung <- sample(laenge,0.8*laenge) # Aufteilung für Test und Trainingsdaten
+Caravan.trainingsdaten <- Caravan[aufteilung,] # Trainingsdaten
+Caravan.testdaten <- Caravan[-aufteilung,] # Testdaten
 
 #------------------------------------
 # Aufgabe 3: Logistische Regression
@@ -55,7 +62,7 @@ glm.fit <- glm(
   data = Caravan.trainingsdaten
 )
 
-glm.fit
+glm.fit # zur Ausgabe
 
 vorhergeseheneWerte <- predict( 
   object  = glm.fit, 
@@ -63,12 +70,12 @@ vorhergeseheneWerte <- predict(
   type    = "response"
 )
 
-# Aus diesen vorhergesehenen Werten, werden nun cutoffPoints definiert. Um sinnvolle Punkte festzustellen,
-# werden die Vorhergesagten Werte (transparent) geplottet, um ihre Verteilung festzustellen
+# Aus diesen vorhergesehenen Werten, werden nun Cutoff Points definiert. Um sinnvolle Punkte festzustellen,
+# werden die Vorhergesagten Werte (transparent) geplottet, damit die Verteilung festgestellt werden kann.
 
 plot(vorhergeseheneWerte, col = adjustcolor("blue", alpha = 0.35))
 
-# Aus diesen Werten wurden nun,nach dem eigenem ermessen der Mitarbeiter Cut-Off points definiert 
+# Aus diesen Werten wurden nun,nach dem eigenem Ermessen der Gruppenmitglieder Cutoff Points definiert.
 # Der Fokus liegt hierbei im Bereich 0-01-0.1, da der Plot aufzeigt, dass hier die meisten Werte zu finden sind.
 
 t <- c(0.01,0.05,0.07, 0.1, 0.2, 0.3)
@@ -81,12 +88,16 @@ for ( i in 1:length(t)) {
   print(wahrheitsMatrix[[i]])
 }
 
+# t = 0.01: 69 von 1017 (ca. 6%) Yes werden auch als Yes erkannt und alle (100%) 148 No's werden auch korrekt als No erkannt. (Keine False Positives) 
+# t = 0.05: 54 von 459 (ca. 11%) Yes werden auch als Yes erkannt und 691 von 706 werden als korrekt negativ erkannt. (15 False Positives)
+# t = 0.07: 46 von 332 (ca. 13,8%) werden als korrekt positiv erkannt. 23 False Positives. 
+# t = 0.1 : 35 von 210 (ca. 16,6%) werden als korrekt positiv erkannt. 34 False Positives.
+# t = 0.2 : 16 von 40 (ca. 40%) werden als korrekt positiv erkannt. 53 False Positives.
+# t = 0.3 : 3 von 14 (ca. 21%) werden als korrekt positiv erkannt. 66 False Positives.
 
 #------------------------------------
 # Aufgabe 4: KNN
 #------------------------------------
-library(class)
-
 #Für die Verwendung in KNN, müssen die Testdaten in data.frames umgewandelt werden. 
 Caravan.scaled = subset(Caravan, select = -c(Purchase) )
 
@@ -97,9 +108,7 @@ Caravan.scaled.trainingsdaten.output <- Caravan[aufteilung, "Purchase"]
 Caravan.scaled.testdaten.output <- Caravan[-aufteilung, "Purchase"]
 set.seed(28)
 
-
-
-#Fertige funktion
+#Fertige Funktion
 knnBerechnen <- function(k){
   return( knn(
     train = Caravan.scaled.trainingsdaten,         ## Input-Variablen der Trainingsdaten      
@@ -111,7 +120,6 @@ knnBerechnen <- function(k){
 }
 
 knnListeBerechnen <- function(vektor){
-  
   knnModellListe <- list()
   knnWahrheitsmatrixListe <- list()
   for(i in 1:length(vektor)){
@@ -121,10 +129,11 @@ knnListeBerechnen <- function(vektor){
   for(i in 1:length(knnModellListe)){
     knnWahrheitsmatrixListe[[i]] <- table(knnModellListe[[i]],Caravan.scaled.testdaten.output)
   }
+  
   return(knnWahrheitsmatrixListe)
 }
 
-#zu verwendene K werte , WErte höher als 7 können nicht verewndet
+# Zu verwendene K-Werte , Werte höher als 7 werden nicht verwendet
 kWerte <- c(1,2,3,4,5,6)
 knnWahrheitsmatrixListe <- knnListeBerechnen(kWerte)
 
@@ -132,13 +141,11 @@ knnWahrheitsmatrixListe <- knnListeBerechnen(kWerte)
 # Aufgabe 5: Modellauswahl
 #------------------------------------
 
-# Ziel laut Aufgabenstellung ist es, möglichst wenige false positives zu erhalten,
+# Ziel laut Aufgabenstellung ist es, möglichst wenige False Positives zu erhalten,
 # um die Arbeitszeit nicht zu verschwenden. Andererseits soll es auch möglichst viele Personen erkennen,
 # die Interesse an einer Police haben, um diese zu verkaufen.
-# Zusammenfassend die Übersicht: 
-# Logistische Regression: 
 
-# Jonas's Backlog
+# Recall und Precision werden berrechnet. Anschließend daraus der F1-Score.
 precision <-  confusion.table.k4[[4]]/(confusion.table.k4[[4]] + confusion.table.k4[[3]])
 recall <- confusion.table.k4[[4]]/(confusion.table.k4[[4]]+confusion.table.k4[[2]])
 f1.score <- 2*(recall*precision)/(recall+precision)
@@ -160,13 +167,14 @@ errechneRecall <- function(matrix){
     matrix[[4]]/(matrix[[4]] + matrix[[2]])
   )
 }
-#funktioniert nur für Werte bis 6 bei Knn
+# Funktioniert nur für Werte bis 6 bei KNN
 bestimmeBestenf1Wert <- function(matrixListe){
-  f1Werte <- vector()# Vektor, in dem alle Werte gespeichert werden
+  f1Werte <- vector() # Vektor, in dem alle Werte gespeichert werden
   
   for ( i in 1:length(matrixListe) ){
     f1Werte <- append(f1Werte,  errechneF1score(matrixListe[[i]]))
   }
+  
   for(i in 1:length(f1Werte)){
     if(f1Werte[i]==   max(f1Werte)){
       cat("Der Index des Maximalen Wertes ist:")
@@ -180,12 +188,19 @@ bestimmeBestenf1Wert <- function(matrixListe){
   }
   
 }
-#Möglichkeit wenn ich Lust habe: automatisch bestimmen, zu wem der zwei es gehört 
-# Verbinden beider Listen 
+# Beide Ergebnisse (Logistische Reg. und KNN werden anschließend zusammengeführt)
 kombinierteListe <- append(wahrheitsMatrix, knnWahrheitsmatrixListe)
 bestimmeBestenf1Wert(kombinierteListe)
-#TODO ERgebnisse hier in TExt kopieren
-#Das BEste Ergebnis ist 0.2 bei erstem X-........
+
+# Die Ergebnisse: 
+# Durch die Funktion bestimmeBestenf1Wert, wird das Ergebnis ausgegeben 
+# Der Maximale Wert ist: 0.256
+# Das  Modell mit dem besten F1 Wert ist:
+# test.vorhersage   
+#                   No     Yes
+#           No      1056   53
+#           Yes     40     16
+# Dabei handelt es sich um ein Modell der logistischen Regression mit t = 0.2
 
 
 
