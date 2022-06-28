3be2f19bad40cdacd501f1e0c4767665a48882c0
Jessi <jessica.fndr@gmail.com>
2022-06-28 11:03:19 +0200

    neues template für neue aufgabe

---
+++ Portfolio_Abgabe2.R
@@ -0,0 +1,78 @@
+#<<<---------- Bemerkung Korrektur --------------->>>
+# -------------
+# Ergebnis:
+# -------------
+# Aufgabe 1: /3
+# Aufgabe 2: /4
+# Aufgabe 3: /5
+# Aufgabe 4: /6
+# Aufgabe 5: /6
+# Aufgabe 6: /6
+# -------------
+# Summe: /30
+#--------------
+#<<<---------------------------------------------->>>
+
+#
+# Template für die Bearbeitung der Übungsaufgaben 7
+#        -- Analyse des BostonHousing-Datensatz -- 
+#
+
+#-----------------
+# Bitte ausfüllen:
+#-----------------
+# Gruppe: Anakin
+# Namen und Matrikelnummern:
+# Jonas Lang, 
+# Simon Baader, 
+# Nina Heyer, 
+# Jessica Fander, 2525677
+
+
+
+
+#-------------------------
+# notwendinge Bibliotheken
+#-------------------------
+library(mlbench) #Datensatz
+library(glmnet)  #Ridge/Lasso-Regression
+
+data("BostonHousing")
+
+
+#------------------------------------
+# Aufgabe 1: Trainings- und Testdaten
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 2: Lineare Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 3: Bias-Variance
+#------------------------------------
+
+#    ...Platz für Ihre Begründung...
+
+#------------------------------------
+# Aufgabe 4: Ridge Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 5: Lasso Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+
+#------------------------------------
+# Aufgabe 6: Vergleich der Ergebnisse
+#------------------------------------
+
+#    ...Platz für Ihren Code und Begründung...
