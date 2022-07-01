a068bbe6130be7832efdfb83721dcf7f983a98e3
LangJonas <71004753+LangJonas@users.noreply.github.com>
2022-07-01 15:38:04 +0200

    Update Portfolio_Abgabe2.R

--- Portfolio_Abgabe2.R
+++ Portfolio_Abgabe2.R
@@ -45,25 +45,88 @@ data("BostonHousing")
 #------------------------------------
 
 #    ...Platz für Ihren Code...
+set.seed(28) # Für bessere Nachvollziehbarkeit wird ein Seed gesetzt.
+laenge <- length(BostonHousing$crim) # Anzahl der Datensätze
+aufteilung <- sample(laenge,0.8*laenge) # Aufteilung für Test und Trainingsdaten
+BostonHousing.trainingsdaten <- BostonHousing[aufteilung,] # Trainingsdaten
+BostonHousing.testdaten<- BostonHousing[-aufteilung,] # Testdaten
+
 
 #------------------------------------
 # Aufgabe 2: Lineare Regression
 #------------------------------------
+lm.fit.all <- lm(
+  formula = medv ~ .,
+  data = BostonHousing.trainingsdaten
+)
 
-#    ...Platz für Ihren Code...
+mqaTraining <- function(modell){
+  return( 
+    mean(
+      (BostonHousing.trainingsdaten$medv - predict(modell,BostonHousing.trainingsdaten ))^2)
+    )
+}
+
+mqaTest <- function(modell){
+  return( 
+    mean(
+      (BostonHousing.testdaten$medv - predict(modell,BostonHousing.testdaten ))^2)
+  )
+}
+
+
+summary(lm.fit.all)
+
+
+
+#Trainingsfehler
+training.mqa.lm <- mqaTraining(lm.fit.all)
+training.mqa.lm
+
+
+
+#Testfehler
+test.mqa.lm <- mqaTest(lm.fit.all)
+test.mqa.lm
+
 
 #------------------------------------
 # Aufgabe 3: Bias-Variance
 #------------------------------------
 
+#over/underfitting 
+plot(BostonHousing$medv)
+abline(reg= lm.fit.all, col ="blue")
+
 #    ...Platz für Ihre Begründung...
+
+#TODO TEXTT_ wie im plot erkenntlich, eine gerade kann es nicht wiederspiegeln, deswegen ein offensichtlicher Fall des underfitting
+#Hier viel besser 
 
 #------------------------------------
 # Aufgabe 4: Ridge Regression
 #------------------------------------
 
 #    ...Platz für Ihren Code...
+# Kommentar: ich habe ehrlich gesagt keine Ahnung ob das richtig ist 
+library(glmnet)
+lambda <- 10^seq( from = 5, to = -3, length = 100)
 
+BostonHousing.trainingsdaten.x <- data.matrix(subset(BostonHousing.trainingsdaten, select = -c(medv) ))
+#y
+BostonHousing.trainingsdaten.y <- BostonHousing.trainingsdaten[, "medv"]
+
+ridge.fit <- cv.glmnet(x= BostonHousing.trainingsdaten.x,y= BostonHousing.trainingsdaten$medv,alpha = 0, lambda =lambda)
+
+#training.mqa.ridge <- mqaTraining(ridge.fit)
+#training.mqa.ridge
+
+#test.mqa.ridge <- mqaTest(ridge.fit)
+#test.mqa.ridge
+
+mean(
+  (BostonHousing.trainingsdaten.y - predict(ridge.fit, BostonHousing.trainingsdaten.x))^2
+  )
 #------------------------------------
 # Aufgabe 5: Lasso Regression
 #------------------------------------
